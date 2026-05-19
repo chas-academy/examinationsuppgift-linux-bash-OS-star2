8203d0de914e902070786258b5fbd90c9b2e1e6a
OS-star2 <olha.shyrokova@chasacademy.se>
2026-05-19 17:38:39 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 #kontrollera att det är root som kör scriptet
-if [ "$UID" -ne 0 ]; then
-    echo "Error! Run as root!"
-    exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: Run as root!"
+    exit 1
 fi
 
 #Skapa for loop för användare
 for user in "$@"; do
     #Skapa en ny användare
-    useradd -m $user
+    useradd -m "$user"
 
     #Skapa katalogstruktur
-    mkdir -p /home/$user/Documents
-    mkdir -p /home/$user/Downloads
-    mkdir -p /home/$user/Work
+    mkdir -p "/home/$user/Documents"
+    mkdir -p "/home/$user/Downloads"
+    mkdir -p "/home/$user/Work"
 
     #Ge rättigheter 
-    chmod 700 /home/$user/Documents
-    chmod 700 /home/$user/Downloads
-    chmod 700 /home/$user/Work
+    chmod 700 "/home/$user/Documents"
+    chmod 700 "/home/$user/Downloads"
+    chmod 700 "/home/$user/Work"
 
     #Skapa välkomstmeddelande
-    echo "Välkommen $user!" > /home/$user/welcome.txt
+    echo "Välkommen $user!" > "/home/$user/welcome.txt"
 
     #Skriva i alla användare till welcome.txt
-    cut -d: -f1 /etc/passwd >> /home/$user/welcome.txt
+    cut -d: -f1 /etc/passwd >> "/home/$user/welcome.txt"
 
     #Kontrollera att ny användare äger de skapade filarna
-    chown -R $user:$user /home/$user
+    chown -R "$user:$user" "/home/$user"
     
 done
 
