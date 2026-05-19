8483bb254329e35003275357eed88a5ce3a68d61
OS-star2 <olha.shyrokova@chasacademy.se>
2026-05-19 17:06:03 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -9,7 +9,7 @@ fi
 #Skapa for loop för användare
 for user in "$@"; do
     #Skapa en ny användare
-    useradd -m $user
+    useradd -m "$user"
 
     #Skapa katalogstruktur
     mkdir /home/$user/Documents
